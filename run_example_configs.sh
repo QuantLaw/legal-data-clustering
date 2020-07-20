3e8319f66f0e9954e3caa1451a64eb3e40585184
Janis Beckedorf <janis@beckfish.de>
2020-07-20 18:55:49 +0200

    Update configs

--- run_example_configs.sh
+++ run_example_configs.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Complexity paper
-python . de preprocess cluster cluster_evolution_graph --number-of-modules 100 --consensus 1000
-python . us preprocess cluster cluster_evolution_graph --number-of-modules 100 --consensus 1000
-
 # Thesis
 
 # Vergleich der Algorithmen
@@ -16,8 +12,14 @@ python . de preprocess cluster --clustering-method louvain --seed 1 --pp-co-occu
 # Verlgeich des Detailgrads
 python . de preprocess cluster --clustering-method louvain --seed 1 --pp-ratio 5 --pp-merge 0 --pp-co-occurrence -2 --pp-co-occurrence-type paragraph --snapshot 2018-01-01 --markov-time 1
 
-# Zeitlich mittel
-python . de preprocess cluster cluster_evolution_graph --clustering-method louvain --consensus 1000  --pp-co-occurrence -2 --pp-co-occurrence-type paragraph --markov-time 0.5
+# Zeitlich mittel / grob
+python . de preprocess cluster cluster_evolution_graph --clustering-method louvain --consensus 1000  --pp-co-occurrence -2 --pp-co-occurrence-type paragraph --markov-time 1 0.5
 
-# Zeitlich grob
-python . de preprocess cluster cluster_evolution_graph --clustering-method louvain --seed 1 --pp-co-occurrence -2 --pp-co-occurrence-type paragraph --consensus 1000
+# Source graph form Steuerrecht zoom in
+python . de preprocess --pp-co-occurrence 0 -1 -2 --pp-co-occurrence-type paragraph --pp-merge 0 --pp-ratio 1 --pp-decay 1 --snapshot 2018-01-01
+
+
+# Complexity paper
+
+python . de preprocess cluster cluster_evolution_graph --number-of-modules 100 --consensus 1000 --clustering-method infomap infomap-directed
+python . us preprocess cluster cluster_evolution_graph --number-of-modules 100 --consensus 1000 --clustering-method infomap infomap-directed
