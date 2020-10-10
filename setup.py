52b6b826d4544bc7547ef777aa8274f30e1fcf14
Janis Beckedorf <31166892+beckedorf@users.noreply.github.com>
2020-10-10 15:18:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     description="Detect communities in legal networks",
     url="git@github.com:QuantLaw/legal-data-clustering.git",
-    author="Corinna Coupette",
+    author="QuantLaw Research Group",
     author_email="",
     license="new-bsd",
     packages=find_namespace_packages(include=["legal_data_clustering.*"]),
